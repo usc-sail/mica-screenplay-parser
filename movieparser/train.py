6de5514b0ec87378172e6158f2d0186eccdbb5a2
sabyasachee <sabyasachee.1301@gmail.com>
2022-04-02 22:45:22 -0700

    print time taken, add verbose option

--- movieparser/train.py
+++ movieparser/train.py
@@ -4,6 +4,7 @@
 import math
 import os
 import sys
+import time
 
 # third party imports
 import numpy as np
@@ -18,19 +19,19 @@ from movieparser.scriptparser import ScriptParser
 from movieparser.evaluate import evaluate, evaluate_movie
 
 def train_movie(movie: str, device:torch.device, results_folder: str, seqlen: int, train_batch_size: int, \
-    eval_batch_size: int, leave_one_movie_out: bool, encoder_learning_rate: float, learning_rate: float, \
-    max_epochs: int, max_norm: int, verbose = False) -> pd.DataFrame:
+    eval_batch_size: int, encoder_learning_rate: float, learning_rate: float, max_epochs: int, max_norm: float, \
+        verbose = False) -> pd.DataFrame:
     
     name = current_process().name
     if movie == "-":
-        print("{}: training on (movie, error) stratified splits".format(name))
+        print("{:25s}: training on (movie, error) stratified splits".format(name))
     else:
-        print("{}: training on all movies except {}".format(name, movie))
+        print("{:25s}: training on all movies except {}".format(name, movie))
 
     train_loader, test_loader, dev_loader = get_dataloaders(results_folder, seqlen, train_batch_size, \
         eval_batch_size, movie, device)
-    print("{}: {} train batches, {} dev batches, {} test batches".format(name, len(train_loader), len(dev_loader), \
-        len(test_loader)))
+    print("{:25s}: {:3d} train batches, {:3d} dev batches, {:3d} test batches".format(name, len(train_loader), \
+        len(dev_loader), len(test_loader)))
 
     feature_size = train_loader.features.shape[2]
     scriptparser = ScriptParser(feature_size, len(label2id))
@@ -43,9 +44,10 @@ def train_movie(movie: str, device:torch.device, results_folder: str, seqlen: in
 
     for epoch in range(max_epochs):
         if verbose:
-            print("{}: epoch {:2d}".format(name, epoch + 1))
+            print("{:25s}: epoch {:2d}".format(name, epoch + 1))
         train_losses = []
         scriptparser.train()
+        start = int(time.time())
 
         for b, (train_scripts, train_features, train_labels) in enumerate(train_loader):
             loss, _ = scriptparser(train_scripts, train_features, train_labels)
@@ -54,23 +56,25 @@ def train_movie(movie: str, device:torch.device, results_folder: str, seqlen: in
             optimizer.step()
             optimizer.zero_grad()
             train_losses.append(loss.detach().cpu().item())
-            if verbose and (b + 1) % 50 == 0:
-                print("{}: \tbatch {:3d} avg. train loss = {:.3f}".format(name, b + 1, np.mean(train_losses)))
+            if (b + 1) % 20 == 0:
+                print("{:25s}: epoch {:2d} batch {:3d} avg. train loss = {:.3f}".format(name, epoch + 1, b + 1, \
+                    np.mean(train_losses)))
 
         dev_perf, dev_loss = evaluate(scriptparser, dev_loader)
         if verbose:
-            print("{}: dev evaluation:".format(name))
+            print("{:25s}: dev evaluation:".format(name))
             print(dev_perf)
-            print()
-        print("{}: epoch {:2d} train loss = {:.3f}, dev loss = {:.3f}".format(name, epoch + 1, np.mean(train_losses), \
-            dev_loss))
+        delta = int(time.time()) - start
+        minutes, seconds = delta//60, delta%60
+        print("{:25s}: epoch {:2d} avg. train loss = {:.3f}, dev loss = {:.3f}, time taken = {:2d} min {:2d} sec"\
+            .format(name, epoch + 1, np.mean(train_losses), dev_loss, minutes, seconds))
 
     test_perf, _ = evaluate(scriptparser, test_loader)
     if verbose:
-        print("{}: test evaluation:".format(name))
+        print("{:25s}: test evaluation:".format(name))
         print(test_perf)
         print()
-    print("{}: F1 S={:.3f}, N={:.3f}, C={:.3f}, D={:.3f}, T={:.3f}, E={:.3f}, M={:.3f}".format(name, \
+    print("{:25s}: F1 S={:.3f}, N={:.3f}, C={:.3f}, D={:.3f}, T={:.3f}, E={:.3f}, M={:.3f}".format(name, \
         test_perf.loc["S", "f1"], test_perf.loc["N", "f1"], test_perf.loc["C", "f1"], test_perf.loc["D", "f1"], \
         test_perf.loc["T", "f1"], test_perf.loc["E", "f1"], test_perf.loc["M", "f1"]))
 
@@ -78,7 +82,7 @@ def train_movie(movie: str, device:torch.device, results_folder: str, seqlen: in
 
 def train(data_folder: str, results_folder: str, seqlen: int, train_batch_size: int, eval_batch_size: int, \
     eval_movie: str, leave_one_movie_out: bool, learning_rate: float, encoder_learning_rate: float, \
-    max_epochs: int, patience: int, max_norm: float, parallel: bool, n_folds_per_gpu: int):
+    max_epochs: int, patience: int, max_norm: float, parallel: bool, n_folds_per_gpu: int, verbose = False):
     
     movies = [line.split()[0] for line in \
         open(os.path.join(data_folder, "SAIL_annotation_screenplays/line_indices.txt")).read().splitlines()]
@@ -98,10 +102,11 @@ def train(data_folder: str, results_folder: str, seqlen: int, train_batch_size: 
         n_gpus = torch.cuda.device_count()
         n_processes = n_gpus * n_folds_per_gpu
     else:
+        n_gpus = 1
         n_processes = 1
     n_iterations = math.ceil(len(movies)/n_processes)
-    arguments = [results_folder, seqlen, train_batch_size, eval_batch_size, leave_one_movie_out, \
-        encoder_learning_rate, learning_rate, max_epochs, max_norm]
+    arguments = [results_folder, seqlen, train_batch_size, eval_batch_size, encoder_learning_rate, learning_rate, \
+        max_epochs, max_norm, verbose]
     test_perfs = []
 
     print("starting multiprocess training:\n")
