84556638e3fff3ecc71828dcb5615e8a1e121875
sabyasachee <sabyasachee.1301@gmail.com>
2022-04-07 01:11:30 -0700

    remove evaluate movie function, change evaluate function to return dictionary

--- movieparser/evaluate.py
+++ movieparser/evaluate.py
@@ -1,25 +1,20 @@
 # author : Sabyasachee
 
 # standard library imports
-from typing import Tuple, List
+from typing import List, Dict, Any
 
 # third party imports
-import numpy as np
 import pandas as pd
 import torch
-from tqdm import tqdm
-from sklearn.metrics import precision_recall_fscore_support, confusion_matrix, f1_score
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 
 # user library imports
 from movieparser.scriptparser import ScriptParser
-from movieparser.scriptloader import ScriptLoader, label2id
+from movieparser.scriptloader import InferenceScriptLoader, label2id
 
-def get_classification_report(label, pred) -> pd.DataFrame:
-    C = confusion_matrix(label, pred, labels=list(range(len(label2id))))
-    precision, recall, f1, support = precision_recall_fscore_support(label, pred, zero_division=0, labels=list(range(len(label2id))))
-    macro_f1 = f1_score(label, pred, zero_division=0, average="macro", labels=list(range(1, len(label2id))))
-    micro_f1 = f1_score(label, pred, zero_division=0, average="micro", labels=list(range(1, len(label2id))))
-    wtavg_f1 = f1_score(label, pred, zero_division=0, average="weighted", labels=list(range(1, len(label2id))))
+def get_classification_report(label: List[str], pred: List[str]) -> pd.DataFrame:
+    C = confusion_matrix(label, pred, labels=list(label2id.keys()))
+    precision, recall, f1, support = precision_recall_fscore_support(label, pred, zero_division=0, labels=list(label2id.keys()))
     id2label = dict((i, label) for label, i in label2id.items())
     labels = [id2label[i] for i in range(C.shape[0])]
     df = pd.DataFrame(C, columns=labels, index=labels)
@@ -27,36 +22,53 @@ def get_classification_report(label, pred) -> pd.DataFrame:
     df["precision"] = precision
     df["recall"] = recall
     df["f1"] = f1
-    df["macro-f1"] = macro_f1
-    df["micro-f1"] = micro_f1
-    df["weighted-f1"] = wtavg_f1
     return df
 
-def evaluate(parser: ScriptParser, loader: ScriptLoader) -> Tuple[pd.DataFrame, float]:
-    parser.eval()
-    label, pred, losses = [], [], []
-    
-    with torch.no_grad():
-        for eval_scripts, eval_features, eval_labels in loader:
-            loss, _pred = parser(eval_scripts, eval_features, eval_labels)
-            label.append(eval_labels)
-            pred.append(_pred)
-            losses.append(loss.cpu().detach().item())
-    
-    label = torch.cat(label).cpu().numpy().astype(int).flatten()
-    pred = torch.cat(pred).cpu().numpy().astype(int).flatten()
-    avg_loss = np.mean(losses)
+def evaluate(parser: ScriptParser, loader: InferenceScriptLoader, process_name: str = "", segment_lengths: List[int] = [10, 50, 100, 1000000]) -> Dict[str, Any]:
+    '''
+    return a dictionary of results indexed by segment lengths \\
+    if segment length is None, key in dictionary is 1000000 (very large number)
 
-    return get_classification_report(label, pred), avg_loss
+    the returned dictionary has the following format: \\
+        segment_length_1:
+            label: List[str]
+            pred: List[str]
+            per_class_performance: pd.DataFrame \\
+            micro:
+                precision: float
+                recall: float
+                f1: float
+        segment_length_2:
+            ...
+    
+    the per_class_performance dataframe is 8 x 12 matrix
+    the row index is O, S, N, C, D, E, T, M
+    the column index is the row index + support, precision, recall, f1
 
-def evaluate_movie(parser: ScriptParser, loader: List[Tuple[List[str], List[List[float]], List[int]]]) -> pd.DataFrame:
+    the micro scores are only for S, N, C, D, E, T
+    we ignore O and M
+    '''
     parser.eval()
-    device = next(parser.parameters()).device
-    label, pred = [], []
+    eval_dict = {}
+
     with torch.no_grad():
-        for eval_scripts, eval_features, eval_labels in tqdm(loader):
-            features = torch.FloatTensor(eval_features).to(device)
-            _pred = parser.parse(eval_scripts, features)
-            label.extend(eval_labels)
-            pred.extend(_pred)
-    return get_classification_report(label, pred)
+        for segment_length in segment_lengths:
+            
+            print("{:25s}: evaluating with segment_length = {}".format(process_name, segment_length))
+
+            if segment_length is None:
+                key = 1000000
+            else:
+                key = segment_length
+            eval_dict[key] = {"label": [], "pred": [], "per_class_performance": None, "micro": None}
+            
+            for eval_scripts, eval_labels in loader:
+                pred = parser.parse(eval_scripts, segment_length=segment_length)
+                eval_dict[key]["label"].extend(eval_labels)
+                eval_dict[key]["pred"].extend(pred)
+    
+            eval_dict[key]["per_class_performance"] = get_classification_report(eval_dict[key]["label"], eval_dict[key]["pred"])
+            p, r, f, _ = precision_recall_fscore_support(eval_dict[key]["label"], eval_dict[key]["pred"], zero_division=0, labels=list("SNCDET"), average="micro")
+            eval_dict[key]["micro"] = {"precision": p, "recall": r, "f1": f}
+    
+    return eval_dict
