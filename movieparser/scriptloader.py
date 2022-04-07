65e69d76a7e223f954e6d3cbfa5f0fab30892d28
sabyasachee <sabyasachee.1301@gmail.com>
2022-04-07 01:20:09 -0700

    create InferenceScriptLoader for the test scripts

--- movieparser/scriptloader.py
+++ movieparser/scriptloader.py
@@ -4,7 +4,8 @@
 from collections import defaultdict
 import math
 import os
-from typing import Tuple
+import sys
+from typing import Tuple, List
 
 # third party imports
 import numpy as np
@@ -13,7 +14,7 @@ import torch
 from tqdm import tqdm
 
 label2id = defaultdict(int)
-for i, label in enumerate("OSNCDTE"):
+for i, label in enumerate("OSNCDETM"):
     label2id[label] = i
 
 class ScriptLoader:
@@ -45,13 +46,35 @@ class ScriptLoader:
             batch_labels = self.labels[batch_index]
             self.i += 1
             return batch_scripts, batch_features, batch_labels
-        else:
-            raise StopIteration
+        raise StopIteration
     
-def get_dataloaders(results_folder: str, seqlen: int, train_batch_size: int, eval_batch_size: int, eval_movie: str, device: torch.device = "cpu") -> Tuple[ScriptLoader, ScriptLoader, ScriptLoader]:
+class InferenceScriptLoader:
+
+    def __init__(self, df: pd.DataFrame, movie: str) -> None:
+        scripts, labels = [], []
+        for _, edf in df[df["movie"] == movie].groupby("error"):
+            scripts.append(edf["text"].fillna("").tolist())
+            labels.append(edf["label"].tolist())
+        self.scripts = scripts
+        self.labels = labels
+
+    def __len__(self) -> int:
+        return len(self.scripts)
+    
+    def __iter__(self):
+        self.i = 0
+        return self
+    
+    def __next__(self) -> Tuple[List[str], List[str]]:
+        if self.i < len(self):
+            self.i += 1
+            return self.scripts[self.i - 1], self.labels[self.i - 1]
+        raise StopIteration
+
+def get_dataloaders(results_folder: str, seqlen: int, train_batch_size: int, eval_movie: str, device: torch.device = "cpu") -> Tuple[ScriptLoader, InferenceScriptLoader]:
 
     df = pd.read_csv(os.path.join(results_folder, "seq_{}.csv".format(seqlen)), index_col=None)
-    df["label"] = df["label"].str.replace("M", "O")
+    data_df = pd.read_csv(os.path.join(results_folder, "data.csv"), index_col=None)
     feats_df = pd.read_csv(os.path.join(results_folder, "feats.csv"), index_col=0)
     features_file = os.path.join(results_folder, "seq_{}_feats.pt".format(seqlen))
     scripts, features, labels = [], [], []
@@ -65,24 +88,21 @@ def get_dataloaders(results_folder: str, seqlen: int, train_batch_size: int, eva
             features.append(feature)
         features = torch.FloatTensor(features)
         torch.save(features, features_file)
-        
-    scripts = df[["line_{}".format(i + 1) for i in range(seqlen)]].values
+    
+
+    scripts = df[["line_{}".format(i + 1) for i in range(seqlen)]].fillna("").values
     labels = df["label"].apply(lambda labelseq: [label2id[label] for label in labelseq])
     
-    scripts = np.array(scripts)
+    scripts = np.array(scripts).astype(str)
     features = features.to(device)
     labels = torch.LongTensor(labels).to(device)
 
-    if eval_movie == "-":
-        train_index = (df["split"] == "train").values
-        test_index = (df["split"] == "test").values
-        dev_index = (df["split"] == "dev").values
-    else:
-        train_index = (df["movie"] != eval_movie).values
-        test_index = dev_index = (df["movie"] == eval_movie).values
+    if eval_movie not in df["movie"].tolist():
+        print("eval movie = {} not in seq_{}.csv".format(eval_movie, seqlen))
+        sys.exit(1)
     
+    train_index = (df["movie"] != eval_movie).values
     train_loader = ScriptLoader(scripts[train_index], features[train_index], labels[train_index], train_batch_size, shuffle=True)
-    test_loader = ScriptLoader(scripts[test_index], features[test_index], labels[test_index], eval_batch_size)
-    dev_loader = ScriptLoader(scripts[dev_index], features[dev_index], labels[dev_index], eval_batch_size)
+    test_loader = InferenceScriptLoader(data_df, eval_movie)
 
-    return train_loader, test_loader, dev_loader
+    return train_loader, test_loader
